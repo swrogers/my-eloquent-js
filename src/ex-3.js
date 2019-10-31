8d35b596ffab392792a6d39630e7e7c02dc640e2
Shane <swrogers@gmail.com>
2019-10-31 14:54:52 -0500

    Chessboard
    Exercise 3 - Make a chessboard

---
+++ src/ex-3.js
@@ -0,0 +1,22 @@
+/**
+ * Exercise 3
+ * Chessboard
+ *
+ * Print out to the console the following:
+ * " # # # #"
+ * "# # # # "
+ * ...
+ * ...
+ *
+ * Then do it for a "size" parameter
+ */
+const size = 8;
+let line = "";
+
+for (let x = 0; x < size; x++) {
+  for (let y = 0; y < size; y++) {
+    (x + y) % 2 ? (line += "#") : (line += " ");
+  }
+  console.log(line);
+  line = "";
+}
