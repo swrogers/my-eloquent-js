d6045be5ddca8cb8f993cd589fa6627f17885a1d
Shane <swrogers@gmail.com>
2019-10-31 13:14:43 -0500

    Forgot comment
    Added exercise description.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-let s = '#';
+/**
+ * Excercise 1
+ * console log 7 tiered triangle
+ */
+let s = "#";
 
-while(s.length < 8) {
+while (s.length < 8) {
   console.log(s);
-  s += '#';
+  s += "#";
 }
