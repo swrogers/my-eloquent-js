d7befcc3eff95d2f4a90728bb52264b7dc0e483d
Shane <swrogers@gmail.com>
2019-11-01 11:58:14 -0500

    Part 1 - Range function
    Create a range function that takes start/end and a step value.

---
+++ src/chapter-4/ex-1.js
@@ -0,0 +1,36 @@
+/**
+ * The sum of a range
+ *
+ * Write a range function that takes two args: start and end
+ * return an array containing number from start to
+ * end inclusive.
+ *
+ * Next write a sum functions that takes an array of
+ * numbers and returns the sum of the numbers.
+ *
+ * Modify range function to take an optional third
+ * argument that indicates the 'step' value to use
+ * when building array. Default to 1. Ensure it works
+ * with negative step values. Only positive integers
+ * should be in the returned array.
+ */
+
+const range = (start, end, step = 1) => {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
+      array.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i += step) {
+      array.push(i);
+    }
+  }
+  return array;
+};
+
+console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(range(0, 10, 2));
+console.log(range(20, 0, -5));
