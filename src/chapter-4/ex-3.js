758237eef7f545eb4b5f13d3ff50b9fa3b48dc18
Shane <swrogers@gmail.com>
2019-11-02 11:39:04 -0500

    Finishing up: listToArray, prepend, nth
    Finished up the final three parts of the exercise.

--- src/chapter-4/ex-3.js
+++ src/chapter-4/ex-3.js
@@ -43,10 +43,43 @@ const arrayToList = array => {
   return list;
 };
 
-const listToArray = list => {};
+const listToArray = list => {
+  let array = [];
 
-const prepend = (value, list) => {};
+  for (let node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
 
-const nth = (list, n) => {};
+  return array;
+};
+
+const prepend = (value, list) => {
+  return {
+    value,
+    rest: list
+  };
+};
+
+const nth = (list, n) => {
+  if (list == null) {
+    return undefined;
+  } else if (n === 0) {
+    return list.value;
+  } else {
+    list = list.rest;
+    n -= 1;
+    return nth(list, n);
+  }
+};
 
 console.log(arrayToList([1, 2, 3]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+
+let a = arrayToList([1, 2, 3, 4, 5]);
+console.log(a);
+console.log(prepend(0, a));
+
+console.log(prepend(10, prepend(20, null)));
+
+console.log(nth(arrayToList([10, 20, 30]), 1));
+console.log(nth(arrayToList([0, 1, 2, 3, 4, 5]), 8));
